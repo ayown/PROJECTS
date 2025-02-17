8815ced3cd5bc1ff7f6da4f4b60a93b405b2d73b
AYAN BHATTACHARJEE <bhattacharjeeayan93@gmail.com>
2025-02-17 19:38:07 +0530

    added converter

---
+++ CURRENCY CONVERTER/app.js
@@ -0,0 +1,13 @@
+const BASE_URL ="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+
+const ddSelect= document.querySelector(".dropdown select");
+
+
+for(let select of ddSelect){
+    for (currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText= currCode;
+        newOption.value= currCode;
+        select.append(newOption);
+    }
+}
